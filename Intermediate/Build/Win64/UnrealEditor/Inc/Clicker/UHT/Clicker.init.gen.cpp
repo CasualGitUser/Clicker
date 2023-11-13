ad24c60a8e842c5b3dfae1f9b551babf26f8b666
Kartoffel69420 <erik1114g@gmail.com>
2023-11-13 16:14:04 +0100

    Add project files.

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Clicker/UHT/Clicker.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeClicker_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_Clicker;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_Clicker()
+	{
+		if (!Z_Registration_Info_UPackage__Script_Clicker.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/Clicker",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x22B326A8,
+				0x4ACABB99,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_Clicker.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_Clicker.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_Clicker(Z_Construct_UPackage__Script_Clicker, TEXT("/Script/Clicker"), Z_Registration_Info_UPackage__Script_Clicker, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x22B326A8, 0x4ACABB99));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/Clicker/UHT/PlayerPawn.gen.cpp
@@ -0,0 +1,89 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "Clicker/Player/PlayerPawn.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodePlayerPawn() {}
+// Cross Module References
+	CLICKER_API UClass* Z_Construct_UClass_APlayerPawn();
+	CLICKER_API UClass* Z_Construct_UClass_APlayerPawn_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_APawn();
+	UPackage* Z_Construct_UPackage__Script_Clicker();
+// End Cross Module References
+	void APlayerPawn::StaticRegisterNativesAPlayerPawn()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(APlayerPawn);
+	UClass* Z_Construct_UClass_APlayerPawn_NoRegister()
+	{
+		return APlayerPawn::StaticClass();
+	}
+	struct Z_Construct_UClass_APlayerPawn_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_APlayerPawn_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_APawn,
+		(UObject* (*)())Z_Construct_UPackage__Script_Clicker,
+	};
+	static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_APlayerPawn_Statics::DependentSingletons) < 16);
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_APlayerPawn_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Navigation" },
+		{ "IncludePath", "Player/PlayerPawn.h" },
+		{ "ModuleRelativePath", "Player/PlayerPawn.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_APlayerPawn_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<APlayerPawn>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_APlayerPawn_Statics::ClassParams = {
+		&APlayerPawn::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009000A4u,
+		METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_APlayerPawn_Statics::Class_MetaDataParams), Z_Construct_UClass_APlayerPawn_Statics::Class_MetaDataParams)
+	};
+	UClass* Z_Construct_UClass_APlayerPawn()
+	{
+		if (!Z_Registration_Info_UClass_APlayerPawn.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_APlayerPawn.OuterSingleton, Z_Construct_UClass_APlayerPawn_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_APlayerPawn.OuterSingleton;
+	}
+	template<> CLICKER_API UClass* StaticClass<APlayerPawn>()
+	{
+		return APlayerPawn::StaticClass();
+	}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(APlayerPawn);
+	APlayerPawn::~APlayerPawn() {}
+	struct Z_CompiledInDeferFile_FID_Clicker_Source_Clicker_Player_PlayerPawn_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Clicker_Source_Clicker_Player_PlayerPawn_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_APlayerPawn, APlayerPawn::StaticClass, TEXT("APlayerPawn"), &Z_Registration_Info_UClass_APlayerPawn, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(APlayerPawn), 115450005U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_Clicker_Source_Clicker_Player_PlayerPawn_h_1519906750(TEXT("/Script/Clicker"),
+		Z_CompiledInDeferFile_FID_Clicker_Source_Clicker_Player_PlayerPawn_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_Clicker_Source_Clicker_Player_PlayerPawn_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
